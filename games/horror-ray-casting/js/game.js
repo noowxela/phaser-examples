16da008b9f6fd3b66d1e546cf0f4825665d5c3d5
alexWoon <noowxela@gmail.com>
2023-08-07 17:45:37 +0800

    Updates

--- games/horror-ray-casting/js/game.js
+++ games/horror-ray-casting/js/game.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var game = new Phaser.Game(640, 480, Phaser.CANVAS, "", {
+  var game = new Phaser.Game(640, 480, Phaser.CANVAS, "game", {
     create: onCreate
   });
   // the canvas where we will show the obstaclesCanvas
